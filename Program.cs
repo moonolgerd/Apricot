8df943cc94f9a66eeb0964f023254a2a4a73469d
Oleg <moonolgerd@gmail.com>
2020-05-05 00:20:38 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Apricot.Services;
+using System.Net.Http;
+
+namespace Apricot
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddSingleton<IApricotService, ApricotService>();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ Services/IApricotService.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Apricot.Services
+{
+    interface IApricotService
+    {
+        IEnumerable<Apricot> GetApricots();
+    }
+
+    public class ApricotService : IApricotService
+    {
+        public IEnumerable<Apricot> GetApricots()
+        {
+            return new List<Apricot>
+            { new Apricot {
+                Name = "Common Apricot",
+                Family = "Apricots"
+            } };
+        }
+    }
+
+    public class Apricot
+    {
+        public string Name { get; set; }
+        public string Family { get; set; }
+    }
+}
