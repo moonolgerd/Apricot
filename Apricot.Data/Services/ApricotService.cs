877e3ec2f0d62ef0073971e26e7674b83fb60457
Oleg <moonolgerd@gmail.com>
2020-05-12 20:28:49 -0400

    Blazor WebAssembly GRPC

---
+++ Apricot.Data/Services/ApricotService.cs
@@ -0,0 +1,18 @@
+﻿using Grpc.Core;
+using System.Threading.Tasks;
+using static Apricot.Data.ApricotService;
+
+namespace Apricot.Data.Services
+{
+    public class ApricotService : ApricotServiceBase
+    {
+        public override Task<ApricotReply> GetApricots(ApricotRequest request, ServerCallContext context)
+        {
+            var reply = new ApricotReply();
+            reply.Apricots.Add(new Apricot { Name = "Apricot", Family = "Apricot" });
+            reply.Apricots.Add(new Apricot { Name = "Red Apricot", Family = "Apricot" });
+            reply.Apricots.Add(new Apricot { Name = "Orange Apricot", Family = "Apricot" });
+            return Task.FromResult(reply);
+        }
+    }
+}
