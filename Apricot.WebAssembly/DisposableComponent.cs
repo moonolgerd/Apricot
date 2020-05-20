e91388cb53a4428692739c1e0dbb77c189f99aaf
Oleg <moonolgerd@gmail.com>
2020-05-19 23:10:37 -0400

    Added toasts

---
+++ Apricot.WebAssembly/DisposableComponent.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Diagnostics;
+
+namespace Apricot
+{
+    [DebuggerDisplay("{" + nameof(GetDebuggerDisplay) + "(),nq}")]
+    public class DisposableComponent : ComponentBase, IDisposable
+    {
+        private bool disposedValue;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+        }
+
+        private string GetDebuggerDisplay()
+        {
+            return ToString();
+        }
+    }
+}
--- Apricot.WebAssembly/Program.cs
+++ Apricot.WebAssembly/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Apricot.Services;
 using System.Net.Http;
+using Blazored.Toast;
 
 namespace Apricot
 {
@@ -19,6 +20,7 @@ namespace Apricot
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             builder.Services.AddSingleton<IApricotService, ApricotService>();
+            builder.Services.AddBlazoredToast();
 
             await builder.Build().RunAsync();
         }
